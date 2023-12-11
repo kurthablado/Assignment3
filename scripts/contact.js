ceff2744460f259eedf335d7571d5f99d785153c
kurthablado <152564970+kurthablado@users.noreply.github.com>
2023-12-10 22:14:08 -0700

    Update contact.js

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,17 +1,16 @@
 // when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
 
-// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+let submit = document.getElementById("submit-button");
 
-document.getElementById("submit-button").addEventListener("submit", message()); {
-    function message () {
-       var thankYou = document.createElement("p");
-       thankYou.write = "Thank you for your message";
-       thankYou.style.fontSize = "24px";
-       document.bodyinnerHTML = "";
-       document.body.appendChild(thankYou);
-    }
-};
+submit.onsubmit = message(); 
 
-// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
+function message() {
+    let thankYou = document.createElement("p");
+    let msg = document.createTextNode("Thank you for your message");
+    thankYou.appendChild(msg);
+    let contactForm = document.getElementById('contact-us');
+    contactForm.replaceChild(thankYou);
+}
+
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
