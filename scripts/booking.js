d876ebc034bee7f6a90afc3f97443869c48930c7
kurthablado <152564970+kurthablado@users.noreply.github.com>
2023-12-10 18:06:42 -0700

    Add files via upload

---
+++ scripts/booking.js
@@ -0,0 +1,49 @@
+/********* create variables *********/
+// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
+// Do any of these variables need to be initialized when the page is loaded? 
+// When do they need to be reset or updated?
+
+let costPerDay = 35
+
+let days_selected;
+
+let clicked_elements;
+
+/********* colour change days of week *********/
+// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
+// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
+
+document.addEventListener('DomContentLoaded', function() {
+    var dayButton = document.getElementsByClassName("blue-hover").addEventListener("click", function() {
+        dayButton.classList.toggle('clicked');
+    })
+})
+
+/********* clear days *********/
+// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+
+function clearButton () {
+    document.querySelectorAll('.blue-hover li').forEach(function(li) {
+        li.classList.remove('clicked');
+    });
+}
+
+
+
+
+/********* change rate *********/
+// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+
+
+
+
+// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+
+
+
+
+
+/********* calculate *********/
+// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
+
+
---
+++ scripts/contact.js
@@ -0,0 +1,16 @@
+// when the "submit-button" is clicked, the contents of the contact-page are replaced with a single <p> element that reads "Thank you for your message" in size 24 font.
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("submit-button").addEventListener("submit", message());
+
+    function message() {
+        var thankYou = document.createElement("p");
+        thankYou.write = "Thank you for your message";
+        thankYou.style.fontSize = "24px";
+
+        document.body.innerHTML = "";
+        document.body.appendChild(thankYou);
+    }
+});
+
+// hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
